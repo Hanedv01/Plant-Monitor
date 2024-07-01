dcc9ef4034b3354cfc78b3a12aba0e52662201b9
Hanedv01 <118539435+Hanedv01@users.noreply.github.com>
2024-07-01 20:17:15 +0200

    Cleaned up unnecessary lines

--- Plant Monitor/boot.py
+++ Plant Monitor/boot.py
@@ -1,11 +1,6 @@
-import lib.keys as k              # Contain all keys used here
 import lib.wifiConnection as wifi # Contains functions to connect/disconnect from WiFi 
-from lib.mqtt import MQTTClient
 
 try:
     ip = wifi.connect()
 except KeyboardInterrupt:
     print("Keyboard interrupt")
-
-#client = MQTTClient(k.AIO_CLIENT_ID, k.AIO_SERVER, k.AIO_PORT, k.AIO_USER, k.AIO_KEY)
-#client.connect()
