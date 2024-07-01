38cd9ed9332a94cbe13c8dcb2c712732e01694d0
Hanedv01 <118539435+Hanedv01@users.noreply.github.com>
2024-07-01 20:07:54 +0200

    Changes to light

--- Plant Monitor/main.py
+++ Plant Monitor/main.py
@@ -1,10 +1,11 @@
 import dht
 import machine
 import time
+import math as m
 from lib.mqtt import MQTTClient   # For use of MQTT protocol to talk to Adafruit IO
 import lib.keys as k
 
-downTime = 5*60
+downTime = 60
 client = MQTTClient(k.AIO_CLIENT_ID, k.AIO_SERVER, k.AIO_PORT, k.AIO_USER, k.AIO_KEY)
 client.connect()
 
@@ -20,14 +21,14 @@ try:
             humidity = tempSensor.humidity()
 
             lightData = lightSensor.read_u16()
-            light = round(1 - lightData / 65535 * 100, 2)
+            light = round(100 - 10*m.log(lightData), 1)    # Arbitrary units
 
             # Do something with the data
-            print(f"The temperature is {temperature} degrees Celsius, the humidity is {humidity} % and the light is {round(light*100)} %.")
+            print(f"The temperature is {temperature} degrees Celsius, the humidity is {humidity} % and the light is {light}.")
 
             client.publish(topic=k.AIO_TEMPERATURE_FEED, msg=str(temperature))
             client.publish(topic=k.AIO_HUMIDITY_FEED, msg=str(humidity))
-            client.publish(topic=k.AIO_LIGHT_FEED, msg=str(light*100))
+            client.publish(topic=k.AIO_LIGHT_FEED, msg=str(light))
             
         except Exception as error:
             print("Exception occurred", error)
